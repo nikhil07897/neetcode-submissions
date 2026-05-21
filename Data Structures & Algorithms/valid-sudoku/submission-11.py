d776ecd2866d430a1cba586fda0dda0bea641561
Nikhil Patil <nikhil07897@gmail.com>
2026-05-21 15:10:14 +0530

    Add: valid-sudoku - submission-11

---
+++ Data Structures & Algorithms/valid-sudoku/submission-11.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[i][j]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+
+        for i in range(9):
+            s = set()
+            for j in range(9):
+                item = board[j][i]
+                if item in s:
+                    return False
+                elif item != '.':
+                    s.add(item)
+        starts = [
+            (0,0), (0,3), (0,6),
+            (3,0), (3,3), (3,6),
+            (6,0), (6,3), (6,6)
+        ]
+
+        for row, col in starts:
+            s = set()
+            for i in range(row, row + 3):
+                for j in range(col, col + 3):
+                    item = board[i][j]
+                    if item in s:
+                        return False
+                    elif item != '.':
+                        s.add(item)
+        return True
+
+        
