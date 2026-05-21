aebd828d86cc0c8c30055ab5e29ff298e2d7985c
Nikhil Patil <nikhil07897@gmail.com>
2026-05-21 19:54:21 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        nstr = ""
+        for c in s:
+            if c.isalnum():
+                nstr += c.lower()
+        return nstr == nstr[::-1]
+        
