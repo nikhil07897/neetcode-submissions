2afd2417421c334826d9803a586d9c1b583f0f8f
Nikhil Patil <nikhil07897@gmail.com>
2026-09-01 17:32:42 +0530

    Add: permutation-string - submission-32

---
+++ Data Structures & Algorithms/permutation-string/submission-32.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        need = Counter(s1)
+        window = Counter()
+        l = 0
+        for r in range(len(s2)):
+            window[s2[r]] += 1
+            if r - l + 1 > len(s1):
+                window[s2[l]] -= 1
+                l += 1
+            if window == need:
+                return True
+        return False
+
+        
