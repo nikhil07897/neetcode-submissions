e2b9ba86a95b33c385e0fd5ac0defbd7c02a4b23
Nikhil Patil <nikhil07897@gmail.com>
2026-09-03 15:02:05 +0530

    Add: car-fleet - submission-6

---
+++ Data Structures & Algorithms/car-fleet/submission-6.py
@@ -0,0 +1,14 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        cars = sorted(zip(position, speed))[::-1]
+
+        fleets = 0
+        prev = 0
+        
+        for pos , spd in cars:
+            time = (target - pos) / spd
+
+            if time > prev:
+                fleets += 1
+                prev = time
+        return fleets        
